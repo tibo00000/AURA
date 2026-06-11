772514adcd9e627642e1831bb0e25436eacf6419
Tibo00000 <thibaud.dubrulle@gmail.com>
2026-06-11 02:15:36 +0200

    build: upgrade Kotlin and Compose compiler to 2.1.0 and KSP to 2.1.0-1.0.29 to resolve ExternalPackageParentPatcherLowering crash

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ plugins {
     // Gradle plugins defined at root level
     id("com.android.application") version "8.5.2" apply false
     id("com.android.library") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.multiplatform") version "2.0.21" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
-    id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
+    id("org.jetbrains.kotlin.multiplatform") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
+    id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
     id("org.jetbrains.compose") version "1.7.0" apply false
     id("androidx.room3") version "3.0.0-alpha01" apply false
 }
